0d6f80828d44993bbe47646bb490d0e62815c944
mumiroMurorin <murorin99@gmail.com>
2025-05-22 12:55:23 +0900

    Delete same function

--- Assets/Scripts/ChartConverter/ChartImporterForRhythmGame.cs
+++ Assets/Scripts/ChartConverter/ChartImporterForRhythmGame.cs
@@ -143,17 +143,3 @@ namespace ChartConvert
     }
 
 }
-public class ChartImporterForRhythmGame : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
